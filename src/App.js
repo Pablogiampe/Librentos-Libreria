9fa40cdf2c568c0d2ec71d2efb91762fece5ad01
Pablo <pablogiampe@gmail.com>
2022-07-28 22:22:38 -0300

    adicion del nav

---
+++ src/App.js
@@ -0,0 +1,17 @@
+;
+import './App.css';
+import NavBar from "./components/NavBar.js"
+
+
+
+function App() {
+  return (
+    <div>
+    <Navbar/>
+    <h1>Tienda de Ceramica</h1>
+    <article>Lorem ipsum dolor, sit amet consectetur adipisicing, elit. Minima ratione, nisi odio, mollitia quae repudiandae fugit cupiditate molestiae. Mollitia, corrupti doloribus eum blanditiis soluta deserunt velit quam recusandae dolor nesciunt.</article></article>
+    </div>
+  ); 
+}
+
+export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+function NavBar(){
+	return (
+		<ul>
+			<li>Tienda de Ceramica</li>
+			<li>Inicio</li>
+			<li>Nosotros</li>
+			<li>Productos</li>
+
+		</ul>
+		)
+}
+
+
+export default NavBar;
